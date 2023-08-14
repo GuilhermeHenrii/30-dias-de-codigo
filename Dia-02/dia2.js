b8f7593358cb1f698e1b01a41943396156b10e8f
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-08-14 16:13:25 -0300

    dia-02

---
+++ Dia-02/dia2.js
@@ -0,0 +1,9 @@
+function retorna(){
+    if(true > false){ 
+        const frase = 'HTML5 | CSS3 | JAVASCRIPT | GIT | GITHUB | NODEJS | MONGODB'
+        const minusculo = frase.toLowerCase();
+        return minusculo;
+    };
+}
+retorna();
+console.log(retorna())
